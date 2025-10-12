3e7e22519b037433f5ad52f57cf27ae5494f3669
AchintyaAkula <achintya.akula@gmail.com>
2025-10-11 18:44:32 -0700

    Fixing massive error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/TurretSubsystem.java
@@ -16,7 +16,7 @@ public class TurretSubsystem implements Subsystem {
     public static TurretSubsystem INSTANCE = new TurretSubsystem();
     private TurretSubsystem() { }
 
-    public static MotorEx turretMotor = new MotorEx("tur");
+    public MotorEx turretMotor = new MotorEx("tur");
     public static ControlSystem turretControl = ControlSystem.builder()
             .posPid(0.1,0,0)
             .build()
