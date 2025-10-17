4dbd458bf43c12664d0ea1733bf3ff8ee24a14e6
AchintyaAkula <achintya.akula@gmail.com>
2025-10-16 19:15:22 -0700

    Fixie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/FlywheelTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/FlywheelTuner.java
@@ -10,22 +10,27 @@ import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.controllable.MotorGroup;
+import dev.nextftc.hardware.impl.MotorEx;
 
 @TeleOp(name="Flywheel Tuner")
 public class FlywheelTuner extends NextFTCOpMode {
     public FlywheelTuner() {
         addComponents(
                 BindingsComponent.INSTANCE,
-                BulkReadComponent.INSTANCE,
-                new SubsystemComponent(FlywheelSystem.INSTANCE)
+                BulkReadComponent.INSTANCE
         );
     }
+
+    public MotorEx leftFlywheelMotor = new MotorEx("fwl").reversed();
+    public MotorEx rightFlywheelMotor = new MotorEx("fwr");
+    public MotorGroup flywheelMotors = new MotorGroup(leftFlywheelMotor, rightFlywheelMotor);
 
     @Override
     public void onStartButtonPressed() {
         Gamepads.gamepad1().rightTrigger().greaterThan(0)
-                .whenTrue(FlywheelSystem.INSTANCE.setPower(Gamepads.gamepad1().rightTrigger().get()))
-                .whenBecomesFalse(FlywheelSystem.INSTANCE.setPower(0))
+                 .whenTrue(() -> flywheelMotors.setPower(Gamepads.gamepad1().rightTrigger().get()))
+                .whenBecomesFalse(() -> flywheelMotors.setPower(0))
         ;
     }
 
