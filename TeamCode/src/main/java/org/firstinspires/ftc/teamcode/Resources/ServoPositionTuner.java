750002b7a98b591b83f0ab434184cdc26bc11ca0
AchintyaAkula <achintya.akula@gmail.com>
2025-10-18 13:50:17 -0700

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Resources/ServoPositionTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Resources/ServoPositionTuner.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.Resources;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
+@TeleOp(group="Tuners", name="Servo Position Tuner")
 public class ServoPositionTuner extends OpMode {
     public static double position = 0.0;
 
