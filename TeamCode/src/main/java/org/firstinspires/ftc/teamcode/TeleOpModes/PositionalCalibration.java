06278ea9cc8f6c2d69fa3036374842ef51382090
dzhao <dzhao2029@gmail.com>
2025-10-18 15:34:29 -0700

    achintya did something iedk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/PositionalCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/PositionalCalibration.java
@@ -7,34 +7,42 @@ import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
 
 @TeleOp(name="Angle and Velocity Calibrator")
 public class PositionalCalibration extends NextFTCOpMode {
     public ServoEx hood = new ServoEx("hood");
+    /*
     public MotorEx fwl = new MotorEx("fwl").reversed();
     public MotorEx fwr = new MotorEx("fwr");
     public MotorGroup fwm = new MotorGroup(fwl, fwr);
+    */
 
     public double low = 0;
-    public double mid = 0;
-    public double high = 0;
+    public double mid = 0.3;
+    public double high = 0.7;
 
     @Override
     public void onStartButtonPressed() {
-        Gamepads.gamepad1().circle().whenBecomesTrue(
+        Gamepads.gamepad1().x().whenTrue(
                 () -> hood.setPosition(low)
         );
-        Gamepads.gamepad1().square().whenBecomesTrue(
+        Gamepads.gamepad1().y().whenTrue(
                 () -> hood.setPosition(mid)
         );
-        Gamepads.gamepad1().triangle().whenBecomesTrue(
+        Gamepads.gamepad1().a().whenTrue(
                 () -> hood.setPosition(high)
         );
+        /*
+        Gamepads.gamepad1().rightTrigger().greaterThan(0)
+                .whenTrue(() -> fwm.setPower(Gamepads.gamepad1().rightTrigger().get()))
+                .whenBecomesFalse(() -> fwm.setPower(0));
+
+         */
     }
 
     @Override
     public void onUpdate() {
-        fwm.setPower(Gamepads.gamepad1().rightTrigger().get()-Gamepads.gamepad1().leftTrigger().get());
-        telemetry.addData("Velocity", fwm.getVelocity());
+        //telemetry.addData("Velocity", fwm.getVelocity());
     }
 }
