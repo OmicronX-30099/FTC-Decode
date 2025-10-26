ba2fae4c9c1280082cc99126e46834b3d00f5af4
AchintyaAkula <achintya.akula@gmail.com>
2025-10-26 13:33:19 -0700

    Finished kotlin conversion Hopefully it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/Constants.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.Constants;
+
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(10)
+            .forwardZeroPowerAcceleration(-22.41609)
+            .lateralZeroPowerAcceleration(-61.34924)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.025, 0, 0.001, 0.4))
+            .headingPIDFCoefficients(new PIDFCoefficients(3.5,3, 0, 0.125))
+            //.drivePIDFCoefficients(new FilteredPIDFCoefficients(0,0,0,0.6,0))
+            //.centripetalScaling(0)
+            ;
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("fr")
+            .rightRearMotorName("br")
+            .leftRearMotorName("bl")
+            .leftFrontMotorName("fl")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(79.70232)
+            .yVelocity(64.929)
+        ;
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(2.5)
+            .strafePodX(-6.375)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pipi")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/AutoTurretTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/AutoTurretTest.kt
@@ -3,19 +3,20 @@ package org.firstinspires.ftc.teamcode.TeleOpModes
 import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.extensions.pedro.PedroComponent
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
 import dev.nextftc.ftc.Gamepads
 import dev.nextftc.ftc.NextFTCOpMode
 import dev.nextftc.ftc.components.BulkReadComponent
-import dev.nextftc.hardware.driving.DriverControlledCommand
 import dev.nextftc.hardware.driving.MecanumDriverControlled
 import dev.nextftc.hardware.impl.MotorEx
+import org.firstinspires.ftc.teamcode.Constants.Constants
 import org.firstinspires.ftc.teamcode.Systems.ShooterSystem
 
 class AutoTurretTest: NextFTCOpMode() {
     init {
         addComponents(
             SubsystemComponent(ShooterSystem),
-            PedroComponent(Constants::createFollower)
+            PedroComponent(Constants::createFollower),
             BulkReadComponent,
             BindingsComponent
         )
@@ -40,6 +41,6 @@ class AutoTurretTest: NextFTCOpMode() {
         drivetrain()
     }
     override fun onUpdate() {
-        ShooterSystem.autoShooter()
+        ShooterSystem.autoShooter(follower.pose)
     }
 }
