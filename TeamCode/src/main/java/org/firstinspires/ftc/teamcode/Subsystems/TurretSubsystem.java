4d8ae93a2d89744da61362e21eb1d7757a4429e6
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-11-01 20:59:49 -0700

    Update turret heading calculation logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretSubsystem.java
@@ -25,8 +25,8 @@ public class TurretSubsystem implements Subsystem {
         double x = currentPos.getX();
         double y = currentPos.getY();
         double heading = currentPos.getHeading();
-        double angle = Math.atan((-48-y)/(72-x));
-        double ticks = Math.round(((angle-heading)/(2*Math.PI)) * 384.5 * (100/24));
+        double angle = Math.atan((144-y)/(144-x));
+        double ticks = Math.round((((heading-90)-angle)/(2*Math.PI)) * 384.5 * (100/24));
         if (Math.abs(ticks-LAST_RECORDED_TICKS) >= 3) {
             turretControl.setGoal(new KineticState(LAST_RECORDED_TICKS));
             LAST_RECORDED_TICKS = ticks;
