0e3d38db9e592e26c8e60c84a7f47da87dd3519c
AchintyaAkula <achintya.akula@gmail.com>
2025-11-11 21:34:24 -0800

    testing stupid motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterSystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/ShooterSystem.kt
@@ -68,8 +68,8 @@ object ShooterSystem: SubsystemGroup
     }
 
     fun calibrateTurretPosition(currPose: Pose) {
-        var angle = atan(-(GOAL_POSE.y+currPose.y)/(GOAL_POSE.x-currPose.x))
-        var ticks = ((-currPose.heading-angle) / (2 * PI)) * (100 / 24) * 384.5+192.5+225;
+        var angle = atan((GOAL_POSE.x-currPose.x)/(GOAL_POSE.y-currPose.y))
+        var ticks = (((currPose.heading-90)+angle) / (2*PI)) * (100/24) * 384.5 * -1
         if (abs(round(ticks) - TARGET_TURRET_HEADING) >= 0) {
             TurretSubsystem.setTurretPosition(ticks)
             TARGET_TURRET_HEADING = ticks
