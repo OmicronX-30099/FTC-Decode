e74c3685ce14ce5c5a0c262377d98563d081f805
AchintyaAkula <achintya.akula@gmail.com>
2025-11-14 22:55:07 -0800

    fixing stuff:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/BlueTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpModes/BlueTeleOp.kt
@@ -56,7 +56,7 @@ class BlueTeleOp: NextFTCOpMode() {
             .whenBecomesTrue { follower.pose = Pose(135.2,8.9,(PI/2)) }
         Gamepads.gamepad1.rightTrigger.greaterThan(0.0).or(Gamepads.gamepad1.leftTrigger.greaterThan(0.0))
             .whenBecomesTrue(IntakeSystem.openGateCommand)
-            .whenTrue(IntakeSystem.intakeCommand(Gamepads.gamepad1.rightTrigger.get()- Gamepads.gamepad1.leftTrigger.get()))
+            .whenTrue{IntakeSystem.intakeMotor.power = (Gamepads.gamepad1.rightTrigger.get()- Gamepads.gamepad1.leftTrigger.get())}
             .whenBecomesFalse(IntakeSystem.closeGateCommand.and(IntakeSystem.intakeCommand(0.0)))
         Gamepads.gamepad1.rightBumper
             .whenBecomesTrue(ShooterSystem.kickCommand)
