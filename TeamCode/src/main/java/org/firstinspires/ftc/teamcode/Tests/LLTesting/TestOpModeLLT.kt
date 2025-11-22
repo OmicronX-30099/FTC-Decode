cb54711059dea6a0aa9b02fc5d4cbd5eedaabe3c
AchintyaAkula <achintya.akula@gmail.com>
2025-11-22 10:11:36 -0800

    Fixing ll test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LLTesting/TestOpModeLLT.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LLTesting/TestOpModeLLT.kt
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.Tests.LLTesting
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.ftc.NextFTCOpMode
 import dev.nextftc.ftc.components.BulkReadComponent
 import org.firstinspires.ftc.teamcode.Draft1.System.ShooterSubsystems.TurretSubsystemLLT
 
+@TeleOp(name="LL Test")
 class TestOpModeLLT: NextFTCOpMode() {
     init {
         addComponents(
