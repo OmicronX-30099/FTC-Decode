b858f29c761efc48089f9663a8ee12b93966d097
AchintyaAkula <achintya.akula@gmail.com>
2025-11-21 16:11:34 -0800

    Fixing errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master/System/ShooterSubsystems/CalculationSubsystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Master/System/ShooterSubsystems/CalculationSubsystem.kt
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode.Master.System.ShooterSubsystems
 import com.pedropathing.geometry.Pose
 import dev.nextftc.core.subsystems.Subsystem
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D
+import org.firstinspires.ftc.teamcode.Util.LimelightStatus
+import kotlin.math.PI
+import kotlin.math.atan2
 import kotlin.properties.Delegates
 import kotlin.math.round
 
 object CalculationSubsystem: Subsystem {
-    val goalPose: Pose = Pose(0,141)
+    val goalPose: Pose = Pose(0.0,141.0)
     fun calculateHoodPosition(botPose: Pose3D?, pedroPose: Pose = Pose(0.0,0.0)): Double {
         lateinit var currPose: Pose
         var targetHoodPose: Double by Delegates.notNull()
@@ -27,11 +30,11 @@ object CalculationSubsystem: Subsystem {
     fun calculateTurretPosition(limelightStatus: LimelightStatus, pedroPose: Pose = Pose(0.0,0.0), currentTurretPos: Double): Double {
         var targetTurretPos: Double by Delegates.notNull()
         if (limelightStatus == LimelightStatus.TARGETS_DETECTED) {
-            targetTurretPos: Double = ((limelightStatus.targetX / 360) * (100 / 24) * 384.5 * -1) + currentTurretPos
+            targetTurretPos = ((limelightStatus.targetX / 360) * (100 / 24) * 384.5 * -1) + currentTurretPos
         } else {
             var angle = atan2(goalPose.x-pedroPose.x,goalPose.y-pedroPose.y)
             targetTurretPos = (((pedroPose.heading-(PI/2))+angle) / (2*PI)) * (100/24) * 384.5 * -1
         }
-        return round(targetTurretPose)
+        return round(targetTurretPos)
     }
 }
