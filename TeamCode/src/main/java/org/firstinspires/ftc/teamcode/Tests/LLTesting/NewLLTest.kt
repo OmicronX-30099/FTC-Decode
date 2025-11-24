e68e15a21a18532cbb8fa14b334f6517bde8ff2f
AchintyaAkula <achintya.akula@gmail.com>
2025-11-24 12:30:09 -0800

    sadf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LLTesting/NewLLTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/LLTesting/NewLLTest.kt
@@ -6,6 +6,7 @@ import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.ftc.ActiveOpMode
 import dev.nextftc.ftc.NextFTCOpMode
 import dev.nextftc.ftc.components.BulkReadComponent
+import dev.nextftc.hardware.impl.MotorEx
 
 @TeleOp(name="new thingy pls work")
 class NewLLTest: NextFTCOpMode() {
@@ -16,6 +17,7 @@ class NewLLTest: NextFTCOpMode() {
         )
     }
     lateinit var ll: Limelight3A
+    var turretMotor: MotorEx = MotorEx("tur")
 
     override fun onInit() {
         ll = ActiveOpMode.hardwareMap.get(Limelight3A::class.java, "Limelight")
@@ -26,7 +28,10 @@ class NewLLTest: NextFTCOpMode() {
     override fun onUpdate() {
         var result = ll.latestResult
         if (result.isValid && result != null) {
-                telemetry.addData("tx", ll.latestResult.tx)
+                telemetry.addData("tx", result.tx)
+                var ticks = ((result.tx)/360) * (100/24) * -1 * 384.5
+                telemetry.addData("ticks calculation", ticks)
+                telemetry.addData("compiled", ticks+turretMotor.currentPosition)
                 telemetry.update()
         }
     }
