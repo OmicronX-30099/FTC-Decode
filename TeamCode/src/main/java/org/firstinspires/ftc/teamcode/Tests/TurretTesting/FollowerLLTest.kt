0453625a26a58a51219966ab5fad5707df7a23a0
AchintyaAkula <achintya.akula@gmail.com>
2025-11-25 12:20:18 -0800

    added ll correction

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TurretTesting/FollowerLLTest.kt
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.Tests.TurretTesting
+
+import com.pedropathing.geometry.Pose
+import com.qualcomm.hardware.limelightvision.LLResult
+import com.qualcomm.hardware.limelightvision.Limelight3A
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.core.components.BindingsComponent
+import dev.nextftc.core.components.SubsystemComponent
+import dev.nextftc.extensions.pedro.PedroComponent
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
+import dev.nextftc.extensions.pedro.PedroDriverControlled
+import dev.nextftc.ftc.Gamepads
+import dev.nextftc.ftc.NextFTCOpMode
+import dev.nextftc.ftc.components.BulkReadComponent
+import dev.nextftc.hardware.driving.DriverControlledCommand
+import kotlin.math.PI
+
+@TeleOp(name="Follower tester with ll correction")
+class FollowerLLTest: NextFTCOpMode() {
+    init {
+        addComponents(
+            BindingsComponent,
+            BulkReadComponent,
+            PedroComponent(Constants::createFollower),
+            SubsystemComponent(RGBSubsystem)
+        )
+    }
+
+    lateinit var ll: Limelight3A
+    lateinit var drivetrain: DriverControlledCommand
+
+    override fun onInit() {
+        ll = hardwareMap.get(Limelight3A::class.java, "Limelight")
+    }
+    override fun onStartButtonPressed() {
+        follower.setStartingPose(Pose(8.7, 8.9, Math.toRadians(90.0)))
+        drivetrain = PedroDriverControlled(
+            -Gamepads.gamepad1.leftStickY,
+            -Gamepads.gamepad1.leftStickX,
+            -Gamepads.gamepad1.rightStickX,
+            true
+        )
+        drivetrain.schedule()
+    }
+    override fun onUpdate() {
+        var result = ll.latestResult
+        if (result != null && result.isValid) {
+            telemetry.addData("Limelight", "Target Detected")
+            follower.setStartingPose(Pose(result.botpose.position.x, result.botpose.position.x, result.botpose.orientation.yaw))
+            telemetry.addData("Limelight", "Reset follower to = " + Pose(result.botpose.position.x, result.botpose.position.x, result.botpose.orientation.yaw).toString())
+        } else {
+            telemetry.addData("Limelight", "No targets detected")
+        }
+        telemetry.addData("Follower","pose = " + follower.pose.toString())
+        telemetry.addData("Follower","x = " + follower.pose.x.toString())
+        telemetry.addData("Follower","y = " + follower.pose.y.toString())
+        telemetry.addData("Follower","heading = " + (follower.pose.heading * (180/ PI)).toString())
+    }
+}
