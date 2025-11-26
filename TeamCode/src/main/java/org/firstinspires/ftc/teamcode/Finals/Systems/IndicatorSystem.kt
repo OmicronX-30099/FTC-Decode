7981e24e75dd1118559a55c8045a9266c4935337
maximusxiao <xiaomaximus@gmail.com>
2025-11-26 13:19:48 -0800

    changed rgb indicator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/Systems/IndicatorSystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/Systems/IndicatorSystem.kt
@@ -4,12 +4,13 @@ import dev.nextftc.core.subsystems.Subsystem
 import dev.nextftc.hardware.impl.ServoEx
 import org.firstinspires.ftc.teamcode.Finals.Systems.ShooterSystems.FlywheelSubsystem
 import org.firstinspires.ftc.teamcode.Finals.Util.RGB
+import kotlin.math.abs
 
 object IndicatorSystem: Subsystem {
     val rgbIndicator: ServoEx = ServoEx("RGBLight1")
 
     override fun periodic() {
-        if (FlywheelSubsystem.flywheeControl.goal.velocity == FlywheelSubsystem.flywheelMotors.velocity) {
+        if (abs(FlywheelSubsystem.flywheeControl.goal.velocity - FlywheelSubsystem.flywheelMotors.velocity)<21.0) {
             rgbIndicator.position = RGB.GREEN.position
         } else {
             rgbIndicator.position = RGB.RED.position
