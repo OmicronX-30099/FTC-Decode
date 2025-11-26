878bd0135476015e7f4ef560b156a537f3b4ef63
AchintyaAkula <achintya.akula@gmail.com>
2025-11-25 18:47:25 -0800

    added offset tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TurretTesting/FollowerLLTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TurretTesting/FollowerLLTest.kt
@@ -53,6 +53,8 @@ class FollowerLLTest: NextFTCOpMode() {
             telemetry.addData("Limelight", "Reset follower to = " + Pose(adjusted_x,adjusted_y,Math.toDegrees(follower.pose.heading)))
             telemetry.addData("llx", (result.botpose.position.y * (100 / 2.54)) + 72)
             telemetry.addData("lly", (result.botpose.position.x * (100 / 2.54) * -1) + 72)
+            telemetry.addData("x-offset", Math.cos(follower.pose.heading) * 4)
+            telemetry.addData("y-offset", Math.sin(follower.pose.heading) * 4)
         } else {
             telemetry.addData("Limelight", "No targets detected")
         }
