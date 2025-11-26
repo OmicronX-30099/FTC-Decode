c2f77e0a64bfa50fd977e017dbed04989c8b7116
AchintyaAkula <achintya.akula@gmail.com>
2025-11-26 11:49:44 -0800

    config tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/Systems/ShooterSystems/TurretSubsystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/Systems/ShooterSystems/TurretSubsystem.kt
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Finals.Systems.ShooterSystems
 
+import com.bylazar.configurables.annotations.Configurable
 import dev.nextftc.control.ControlSystem
 import dev.nextftc.control.feedback.PIDCoefficients
 import dev.nextftc.core.subsystems.Subsystem
 import dev.nextftc.hardware.impl.MotorEx
 
+@Configurable
 object TurretSubsystem: Subsystem {
     val turretMotor: MotorEx = MotorEx("turretMotor")
 
