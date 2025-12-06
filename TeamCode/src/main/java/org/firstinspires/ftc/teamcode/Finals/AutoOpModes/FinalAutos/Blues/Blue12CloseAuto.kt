61d3ab8b222187bc402a35dee3701f6ba4a4e23e
AchintyaAkula <achintya.akula@gmail.com>
2025-12-05 20:22:58 -0800

    Disabled other old one as well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/AutoOpModes/FinalAutos/Blues/Blue12CloseAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/AutoOpModes/FinalAutos/Blues/Blue12CloseAuto.kt
@@ -5,6 +5,7 @@ import com.pedropathing.geometry.BezierLine
 import com.pedropathing.geometry.Pose
 import com.pedropathing.paths.PathChain
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import dev.nextftc.core.commands.delays.Delay
 import dev.nextftc.core.commands.groups.SequentialGroup
 import dev.nextftc.core.components.SubsystemComponent
@@ -21,6 +22,7 @@ import org.firstinspires.ftc.teamcode.Finals.Systems.ShooterSystem
 import org.firstinspires.ftc.teamcode.Finals.TeleOpModes.BlueTeleOpV1
 import org.firstinspires.ftc.teamcode.Finals.Util.Alliance
 
+@Disabled
 @Autonomous(name="Blue 12 ball, Close Start",group="Finals")
 class Blue12CloseAuto: NextFTCOpMode() {
     init {
