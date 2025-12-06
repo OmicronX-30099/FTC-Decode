09c6d2de3b7517d3bef19638ddf0e44f6728b7f5
= <xiaomaximus@gmail.com>
2025-12-06 11:24:38 -0800

    Added red close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/AutoOpModes/Blue/BlueCloseAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Finals/AutoOpModes/Blue/BlueCloseAuto.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Finals.AutoOpModes.Red
+package org.firstinspires.ftc.teamcode.Finals.AutoOpModes.Blue
 
 import com.pedropathing.geometry.BezierCurve
 import com.pedropathing.geometry.BezierLine
@@ -17,11 +17,12 @@ import org.firstinspires.ftc.teamcode.Finals.Constants
 import org.firstinspires.ftc.teamcode.Finals.Systems.IndicatorSystem
 import org.firstinspires.ftc.teamcode.Finals.Systems.PassiveSystem
 import org.firstinspires.ftc.teamcode.Finals.Systems.ShooterSystem
+import org.firstinspires.ftc.teamcode.Finals.TeleOpModes.BlueTeleOpV1
 import org.firstinspires.ftc.teamcode.Finals.TeleOpModes.RedTeleOpV1
 import org.firstinspires.ftc.teamcode.Finals.Util.Alliance
 
-@Autonomous(name="Red Close Auto",group="Red Final")
-class RedCloseAuto: NextFTCOpMode() {
+@Autonomous(name="Blue Close Auto",group="Blue Final")
+class BlueCloseAuto: NextFTCOpMode() {
     init {
         addComponents(
             BulkReadComponent,
@@ -34,10 +35,10 @@ class RedCloseAuto: NextFTCOpMode() {
     var Paths: Array<PathChain> = arrayOf()
 
     override fun onInit() {
-        ShooterSystem.setAlliance(Alliance.RED)
+        ShooterSystem.setAlliance(Alliance.BLUE)
         PedroComponent.Companion.follower.setStartingPose(
             Pose(
-                Constants.fieldWidth - 32.9,
+                 32.9,
                 131.3,
                 Math.toRadians(0.0)
             )
@@ -90,105 +91,105 @@ class RedCloseAuto: NextFTCOpMode() {
             .pathBuilder()
             .addPath(
                 BezierLine(
-                    Pose(Constants.fieldWidth - 32.900, 131.300),
-                    Pose(Constants.fieldWidth - 58.750, 84.000)
+                    Pose( 32.900, 131.300),
+                    Pose( 58.750, 84.000)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val pickupFirst = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierLine(
-                    Pose(Constants.fieldWidth - 58.750, 84.000),
-                    Pose(Constants.fieldWidth - 17.000, 84.000)
+                    Pose( 58.750, 84.000),
+                    Pose( 17.000, 84.000)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val openGate = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 17.000, 84.000),
-                    Pose(Constants.fieldWidth - 38.000, 80.500),
-                    Pose(Constants.fieldWidth - 17.000, 73.000)
+                    Pose( 17.000, 84.000),
+                    Pose( 38.000, 80.500),
+                    Pose( 17.000, 73.000)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val shootFirst = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierLine(
-                    Pose(Constants.fieldWidth - 17.000, 73.000),
-                    Pose(Constants.fieldWidth - 58.750, 73.000)
+                    Pose( 17.000, 73.000),
+                    Pose( 58.750, 73.000)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val pickupSecond = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 58.750, 73.000),
-                    Pose(Constants.fieldWidth - 39.500, 54.393),
-                    Pose(Constants.fieldWidth - 10.000, 58.500)
+                    Pose( 58.750, 73.000),
+                    Pose( 39.500, 54.393),
+                    Pose( 10.000, 58.500)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val shootSecond = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 10.000, 58.500),
-                    Pose(Constants.fieldWidth - 39.500, 54.393),
-                    Pose(Constants.fieldWidth - 58.750, 73.000)
+                    Pose( 10.000, 58.500),
+                    Pose( 39.500, 54.393),
+                    Pose( 58.750, 73.000)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val pickupThird = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 58.750, 73.000),
-                    Pose(Constants.fieldWidth - 63.075, 30.000),
-                    Pose(Constants.fieldWidth - 10.000, 35.250)
+                    Pose( 58.750, 73.000),
+                    Pose( 63.075, 30.000),
+                    Pose( 10.000, 35.250)
                 )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(0.0))
+            .setConstantHeadingInterpolation(Math.toRadians(180.0))
             .build()
 
         val shootThird = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 10.000, 35.250),
-                    Pose(Constants.fieldWidth - 46.100, 38.300),
-                    Pose(Constants.fieldWidth - 58.750, 73.000)
+                    Pose( 10.000, 35.250),
+                    Pose( 46.100, 38.300),
+                    Pose( 58.750, 73.000)
                 )
             )
-            .setLinearHeadingInterpolation(Math.toRadians(0.0), Math.toRadians(-90.0))
+            .setLinearHeadingInterpolation(Math.toRadians(180.0), Math.toRadians(-90.0))
             .build()
 
         val leavePath = PedroComponent.Companion.follower
             .pathBuilder()
             .addPath(
                 BezierCurve(
-                    Pose(Constants.fieldWidth - 58.750, 73.000),
-                    Pose(Constants.fieldWidth - 51.100, 58.250),
-                    Pose(Constants.fieldWidth - 23.500, 53.000)
+                    Pose( 58.750, 73.000),
+                    Pose( 51.100, 58.250),
+                    Pose( 23.500, 53.000)
                 )
             )
-            .setLinearHeadingInterpolation(Math.toRadians(-90.0), Math.toRadians(180.0))
+            .setLinearHeadingInterpolation(Math.toRadians(-90.0), Math.toRadians(0.0))
             .build()
 
         Paths += scorePreload
@@ -202,6 +203,6 @@ class RedCloseAuto: NextFTCOpMode() {
         Paths += leavePath
     }
     override fun onStop() {
-        RedTeleOpV1.Companion.startPose = PedroComponent.Companion.follower.pose //stores robot position for teleop usage
+        BlueTeleOpV1.Companion.startPose = PedroComponent.Companion.follower.pose //stores robot position for teleop usage
     }
 }
