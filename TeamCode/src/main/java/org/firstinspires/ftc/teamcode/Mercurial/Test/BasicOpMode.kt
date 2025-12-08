149fc438f6b0617b93fe47af0aa59c2d613068a9
AchintyaAkula <achintya.akula@gmail.com>
2025-12-07 17:55:02 -0800

    Added namings and groupings and refactored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mercurial/Test/BasicOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mercurial/Test/BasicOpMode.kt
@@ -12,65 +12,69 @@ import dev.frozenmilk.dairy.mercurial.ftc.Mercurial
 import kotlin.math.abs
 import kotlin.math.max
 
-val basicMercurialTeleop = Mercurial.teleop {
-    val frontLeftMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "frontLeftDriveMotor")
-    val frontRightMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "frontRightDriveMotor")
-    val backLeftMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "backLeftDriveMotor")
-    val backRightMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "backRightDriveMotor")
+val basicMercurialTeleop = Mercurial.teleop(
+    "Mercurial basic TeleOp test",
+    "Mercurial Tests",
+    {
+        val frontLeftMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "frontLeftDriveMotor")
+        val frontRightMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "frontRightDriveMotor")
+        val backLeftMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "backLeftDriveMotor")
+        val backRightMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "backRightDriveMotor")
 
-    frontLeftMotor.direction = DcMotorSimple.Direction.REVERSE
-    backLeftMotor.direction = DcMotorSimple.Direction.REVERSE
+        frontLeftMotor.direction = DcMotorSimple.Direction.REVERSE
+        backLeftMotor.direction = DcMotorSimple.Direction.REVERSE
 
-    var drivetrainScalar = 1.0
+        var drivetrainScalar = 1.0
 
-    val intakeMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "intakeMotor")
+        val intakeMotor: DcMotorEx = hardwareMap.get(DcMotorEx::class.java, "intakeMotor")
 
-    val drivetrain: Closure =
-        exec {
-            val drivePower: Double = -gamepad1.left_stick_y.toDouble()//.pow(2)
-            val strafePower: Double = gamepad1.left_stick_x.toDouble()//.pow(2)
-            val turnPower: Double = gamepad1.right_stick_x.toDouble()//.pow(2)
+        val drivetrain: Closure =
+            exec {
+                val drivePower: Double = -gamepad1.left_stick_y.toDouble()//.pow(2)
+                val strafePower: Double = gamepad1.left_stick_x.toDouble()//.pow(2)
+                val turnPower: Double = gamepad1.right_stick_x.toDouble()//.pow(2)
 
-            val powerLimit: Double = max(abs(drivePower) + abs(strafePower) + abs(turnPower), 1.0)
+                val powerLimit: Double = max(abs(drivePower) + abs(strafePower) + abs(turnPower), 1.0)
 
-            val frontLeftDrivePower: Double = (drivePower + strafePower + turnPower) / powerLimit
-            val frontRightDrivePower: Double = (drivePower - strafePower + turnPower) / powerLimit
-            val backLeftDrivePower: Double = (drivePower - strafePower - turnPower) / powerLimit
-            val backRightDrivePower: Double = (drivePower + strafePower - turnPower) / powerLimit
+                val frontLeftDrivePower: Double = (drivePower + strafePower + turnPower) / powerLimit
+                val frontRightDrivePower: Double = (drivePower - strafePower + turnPower) / powerLimit
+                val backLeftDrivePower: Double = (drivePower - strafePower - turnPower) / powerLimit
+                val backRightDrivePower: Double = (drivePower + strafePower - turnPower) / powerLimit
 
-            frontLeftMotor.power = frontLeftDrivePower * drivetrainScalar
-            frontRightMotor.power = frontRightDrivePower * drivetrainScalar
-            backLeftMotor.power = backLeftDrivePower * drivetrainScalar
-            backRightMotor.power = backRightDrivePower * drivetrainScalar
-        }
-    val intake: Closure =
-        exec {
-            val intakePower: Double = (gamepad1.right_trigger - gamepad1.left_trigger).toDouble()
-            intakeMotor.power = intakePower
-        }
+                frontLeftMotor.power = frontLeftDrivePower * drivetrainScalar
+                frontRightMotor.power = frontRightDrivePower * drivetrainScalar
+                backLeftMotor.power = backLeftDrivePower * drivetrainScalar
+                backRightMotor.power = backRightDrivePower * drivetrainScalar
+            }
+        val intake: Closure =
+            exec {
+                val intakePower: Double = (gamepad1.right_trigger - gamepad1.left_trigger).toDouble()
+                intakeMotor.power = intakePower
+            }
 
-    val drivetrainSpeedControl: Closure =
-        match { drivetrainScalar }
-            .branch(1.0, exec { drivetrainScalar = 0.6 })
-            .branch(0.6, exec { drivetrainScalar = 0.2 })
-            .branch(0.2, exec { drivetrainScalar = 1.0 })
-            .assertExhaustive()
+        val drivetrainSpeedControl: Closure =
+            match { drivetrainScalar }
+                .branch(1.0, exec { drivetrainScalar = 0.6 })
+                .branch(0.6, exec { drivetrainScalar = 0.2 })
+                .branch(0.2, exec { drivetrainScalar = 1.0 })
+                .assertExhaustive()
 
-    schedule(
-        sequence(
-            wait { inLoop },
-            loop(
-                sequence(
-                    drivetrain,
-                    intake
+        schedule(
+            sequence(
+                wait { inLoop },
+                loop(
+                    sequence(
+                        drivetrain,
+                        intake
+                    )
                 )
             )
         )
-    )
-    bindSpawn (
-        risingEdge { gamepad1.right_stick_button },
-        drivetrainSpeedControl
-    )
+        bindSpawn (
+            risingEdge { gamepad1.right_stick_button },
+            drivetrainSpeedControl
+        )
 
-    dropToScheduler()
-}
+        dropToScheduler()
+    }
+)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mercurial/Test/MercurialPedroDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mercurial/Test/MercurialPedroDrive.kt
@@ -13,48 +13,52 @@ import org.firstinspires.ftc.teamcode.Mercurial.Constants
 import java.lang.Math.toRadians
 import kotlin.properties.Delegates
 
-val mercurialPedroDrive = Mercurial.teleop {
-    var follower: Follower by Delegates.notNull()
-    var driveScalar: Double = 1.0
+val mercurialPedroDrive = Mercurial.teleop(
+    "Mercurial Pedro drivetrain test",
+    "Mercurial Tests",
+    {
+        var follower: Follower by Delegates.notNull()
+        var driveScalar: Double = 1.0
 
-    val initializeFollower: Closure =
-        sequence(
-            exec { follower = Constants.createFollower(hardwareMap) },
-            exec { follower.setStartingPose(Pose(70.5,70.5,toRadians(90.0))) },
-            exec { follower.update() }
-        )
-    val updateFollower: Closure =
-        sequence (
-            exec { follower.update() },
-            exec { follower.setTeleOpDrive(
-                -gamepad1.left_stick_y.toDouble() * driveScalar,
-                -gamepad1.left_stick_x.toDouble() * driveScalar,
-                -gamepad1.right_stick_x.toDouble() * driveScalar,
-                true
-            ) }
-        )
-    val drivetrainSpeedControl: Closure =
-        match { driveScalar }
-            .branch(1.0, exec { driveScalar = 0.6 })
-            .branch(0.6, exec { driveScalar = 0.2 })
-            .branch(0.2, exec { driveScalar = 1.0 })
-            .assertExhaustive()
+        val initializeFollower: Closure =
+            sequence(
+                exec { follower = Constants.createFollower(hardwareMap) },
+                exec { follower.setStartingPose(Pose(70.5,70.5,toRadians(90.0))) },
+                exec { follower.update() }
+            )
+        val updateFollower: Closure =
+            sequence (
+                exec { follower.update() },
+                exec { follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y.toDouble() * driveScalar,
+                    -gamepad1.left_stick_x.toDouble() * driveScalar,
+                    -gamepad1.right_stick_x.toDouble() * driveScalar,
+                    true
+                ) }
+            )
+        val drivetrainSpeedControl: Closure =
+            match { driveScalar }
+                .branch(1.0, exec { driveScalar = 0.6 })
+                .branch(0.6, exec { driveScalar = 0.2 })
+                .branch(0.2, exec { driveScalar = 1.0 })
+                .assertExhaustive()
 
-    schedule(
-        sequence(
-            wait { inInit },
-            initializeFollower,
-            wait { inLoop },
-            exec { follower.startTeleopDrive() },
-            loop(
-                updateFollower
+        schedule(
+            sequence(
+                wait { inInit },
+                initializeFollower,
+                wait { inLoop },
+                exec { follower.startTeleopDrive() },
+                loop(
+                    updateFollower
+                )
             )
         )
-    )
-    bindSpawn (
-        risingEdge { gamepad1.right_stick_button },
-        drivetrainSpeedControl
-    )
+        bindSpawn (
+            risingEdge { gamepad1.right_stick_button },
+            drivetrainSpeedControl
+        )
 
-    dropToScheduler()
-}
+        dropToScheduler()
+    }
+)
