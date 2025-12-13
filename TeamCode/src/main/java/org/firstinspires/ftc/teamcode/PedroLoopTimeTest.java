5f82f668baf4db663b505ceb5b71a536d1344618
AchintyaAkula <achintya.akula@gmail.com>
2025-12-12 17:50:41 -0800

    Stupid error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroLoopTimeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroLoopTimeTest.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp("Pedro tester", group="Loop Tests")
+@TeleOp(name="Pedro tester", group="Loop Tests")
 public class PedroLoopTimeTest extends OpMode {
     private ElapsedTime elapsedtime;
     private Follower follower;
