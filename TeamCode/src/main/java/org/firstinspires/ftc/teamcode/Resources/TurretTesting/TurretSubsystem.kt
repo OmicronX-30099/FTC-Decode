3502fe5ac1ff11123267a10c5fb3bc66e7fceea4
AchintyaAkula <achintya.akula@gmail.com>
2025-12-19 08:00:58 -0800

    Caching changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Resources/TurretTesting/TurretSubsystem.kt
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Resources.TurretTesting
+
+import com.bylazar.configurables.annotations.Configurable
+import com.pedropathing.geometry.Pose
+import dev.nextftc.control.ControlSystem
+import dev.nextftc.control.feedback.PIDCoefficients
+import dev.nextftc.control.feedforward.BasicFeedforwardParameters
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
+import dev.nextftc.hardware.impl.MotorEx
+
+@Configurable
+object TurretSubsystem: Subsystem {
+    val turretMotor: MotorEx = MotorEx("turretMotor")
+
+    @JvmField var turretPID = PIDCoefficients(0.0135,0.0,0.0)
+    @JvmField var turretFF = BasicFeedforwardParameters(0.0,0.0,0.0)
+    val turretControl: ControlSystem = ControlSystem.builder()
+        .posPid(turretPID)
+        .basicFF(turretFF)
+        .build()
+
+    fun calculateAndSetTarget(robotPose: Pose, robotVel: Double) {
+        robotPose + follower.velocity.times(0.65)
+    }
+
+    override fun periodic() {
+        turretMotor.power = turretControl.calculate(turretMotor.state)
+    }
+}
